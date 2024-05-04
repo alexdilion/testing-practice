bbbd3af7ab235559f0483654c2847a089b854f39
alexdilion <alexdilion@protonmail.com>
2024-05-04 12:24:11 +0100

    Add reverseString edge test

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -7,9 +7,14 @@ test("Regular strings", () => {
         ["hello", "olleh"],
         ["Reversed", "desreveR"],
         ["Kayak", "kayaK"],
+        ["12345", "54321"],
     ];
 
     tests.forEach((test) => {
         expect(reverseString(test[0])).toMatch(test[1]);
     });
+});
+
+test("Empty string", () => {
+    expect(reverseString("")).toMatch("");
 });
